555862ac838221eb94418f644edf7786e2da833f
RodolfoHerman <ciencia.rodolfo@gmail.com>
2018-05-14 13:26:32 -0300

    Tópico 7, Jquery com Angular

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { FotoService } from '../foto/foto.service';
+import { PainelComponent } from '../painel/painel.component';
 
 @Component({
     moduleId: module.id,
@@ -30,21 +31,29 @@ export class ListagemComponent {
         //     );
     }
 
-    remove(foto: FotoComponent) {
+    remove(foto: FotoComponent, painel: PainelComponent) {
 
         this.service
             .remover(foto)
             .subscribe(
                 () => {
                     
-                    let indice = this.fotos.indexOf(foto);
-                    let aux = this.fotos.slice(0);
-                    
-                    aux.splice(indice, 1);
-                    //Angular só monitora a referência de this.fotos do nosso componente. Se alguém incluir ou remover um novo item da lista ele não saberá. Para isso, é necessário criar uma nova lista e atribuir essa lista em this.fotos. Como estamos reatribuindo um valor para a variável o Angular desencadeará seu mecanismo de deteção de mudança e renderizará a view. (Change Detection)
-                    this.fotos = aux;
+                    //Chamando o Jquery para realizar o fade out
+                    painel.fadeOut(
+                        () => {
+                            
+                            let indice = this.fotos.indexOf(foto);
+                            let aux = this.fotos.slice(0);
+                            
+                            aux.splice(indice, 1);
+                            //Angular só monitora a referência de this.fotos do nosso componente. Se alguém incluir ou remover um novo item da lista ele não saberá. Para isso, é necessário criar uma nova lista e atribuir essa lista em this.fotos. Como estamos reatribuindo um valor para a variável o Angular desencadeará seu mecanismo de deteção de mudança e renderizará a view. (Change Detection)
+                            this.fotos = aux;
+        
+                            this.mensagem = "Foto removida com sucesso !!";
+                        }
+                    );
 
-                    this.mensagem = "Foto removida com sucesso !!";
+
                 },
                 error => {
                     this.mensagem = error;
--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -10,10 +10,18 @@ export class PainelComponent implements OnInit {
     
     @Input() titulo: string;
 
+    //Um elemento do tipo ElementRef encapsula o elemento do DOM do nosso componente na propriedade nativeElement. Quando o painel for criado o Angular irá passar para o construtor do componente o elemento DOM que ele está associado
+    constructor(private elemento: ElementRef) {}
+
     ngOnInit() {
         this.titulo = this.titulo.length > 7 ?
              this.titulo.substr(0, 7) + '...' : 
              this.titulo;
+    }
+
+    fadeOut(cb) {
+        //O elemento é um objeto TypeScript e o 'nativeElement' é o DOM
+        $(this.elemento.nativeElement).fadeOut(cb);
     }
     
 }
